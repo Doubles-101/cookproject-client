dfbd5176c5b4bcc39a93df436aa2a37d9f8c17c8
William Woodard <williamwoodard98@gmail.com>
2024-07-19 15:59:53 -0500

    details are retrieved and a few are displayed

--- src/components/RecipeDetail/RecipeDetail.jsx
+++ src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 
 
 export const RecipeDetail = () => {
+    const [currentRecipe, setCurrentRecipe] = useState({})
+    const { recipeId } = useParams()
+
+    const fetchCurrentRecipe = async () => {
+        const response = await fetch(`http://localhost:8000/recipes/${recipeId}`, {
+            headers: {
+                Authorization: `Token ${JSON.parse(localStorage.getItem("cook_token")).token}`
+            }
+        })
+        const recipe = await response.json()
+        setCurrentRecipe(recipe)
+    }
+
+    useEffect(() => {
+        fetchCurrentRecipe()
+    }, [])
+
+
     return (
-        <>Detail of recipe</>
+        <div>
+            <div>{currentRecipe.title}</div>
+            <div>{currentRecipe.description}</div>
+            <div>{currentRecipe.instructions}</div>
+        </div>
     )
 }
