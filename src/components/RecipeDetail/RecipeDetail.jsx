5e2f17bd220ff32ca4ef30ae315fbbf83954104b
William Woodard <williamwoodard98@gmail.com>
2024-07-22 09:40:18 -0500

    users can delete their own recipes

--- src/components/RecipeDetail/RecipeDetail.jsx
+++ src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 
 
 export const RecipeDetail = () => {
     const [currentRecipe, setCurrentRecipe] = useState({})
     const { recipeId } = useParams()
+    const navigate = useNavigate()
+
+    const customerId = JSON.parse(localStorage.getItem("cook_token")).id
 
     const fetchCurrentRecipe = async () => {
         const response = await fetch(`http://localhost:8000/recipes/${recipeId}`, {
@@ -15,6 +18,21 @@ export const RecipeDetail = () => {
         })
         const recipe = await response.json()
         setCurrentRecipe(recipe)
+    }
+
+    const deleteCurrentRecipe = async () => {
+        await fetch(`http://localhost:8000/recipes/${recipeId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Token ${JSON.parse(localStorage.getItem("cook_token")).token}`,
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
+    const handleDelete = async () => {
+        await deleteCurrentRecipe()
+        navigate("/myRecipeList")
     }
 
     useEffect(() => {
@@ -30,6 +48,17 @@ export const RecipeDetail = () => {
             <div>Categories:  {currentRecipe.categories?.map(category => <div key={category.id}>{category.name}</div>)}</div>
             <div>Ingredients:  <div>{currentRecipe.ingredients}</div></div>
             <div>{currentRecipe.instructions}</div>
+            <div>
+                {customerId == currentRecipe.customer &&
+                    <div>
+                        <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 
+                        hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</button>
+                        <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 
+                        hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-4"
+                        onClick={handleDelete}
+                        >Delete</button>
+                    </div>}
+            </div>
         </div>
     )
 }
