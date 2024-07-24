50ae1f2f7ea9a54495473b4c3f55375f3f0bfb87
William Woodard <williamwoodard98@gmail.com>
2024-07-24 10:04:43 -0500

    added search to recipelist

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -4,9 +4,20 @@ import { Link } from 'react-router-dom'
 
 export const RecipeList = () => {
     const [allRecipes, setAllRecipes] = useState([])
+    const [searchText, setSearchText] = useState("")
 
     const fetchAllRecipes = async () => {
         const response = await fetch(`http://localhost:8000/recipes`, {
+            headers: {
+                Authorization: `Token ${JSON.parse(localStorage.getItem("cook_token")).token}`
+            }
+        })
+        const recipes = await response.json()
+        setAllRecipes(recipes)
+    }
+
+    const fetchSearchedRecipes = async () => {
+        const response = await fetch(`http://localhost:8000/recipes?q=${searchText}`, {
             headers: {
                 Authorization: `Token ${JSON.parse(localStorage.getItem("cook_token")).token}`
             }
@@ -19,9 +30,22 @@ export const RecipeList = () => {
         fetchAllRecipes()
     }, [])
 
+    useEffect(() => {
+        fetchSearchedRecipes()
+    }, [searchText])
+
     return (
         <div className="recipelist-container">
             <div className="header"><h1>All Recipes: </h1></div>
+            <div>
+                <input 
+                    type="text"
+                    name="search"
+                    value={searchText}
+                    onChange={(event) => setSearchText(event.target.value)}
+                    placeholder="Search"
+                />
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {
                     allRecipes.map(recipe => <div key={`key-${recipe.id}`} className="border p-5 border-solid hover:bg-blue-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
