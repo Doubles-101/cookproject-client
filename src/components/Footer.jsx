eff871c8dd67c761108d97e4977b00a70e384f45
William Woodard <williamwoodard98@gmail.com>
2024-07-29 13:58:28 -0500

    added a basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-gray-600 text-white py-6">
+            <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
+                <div className="text-sm">
+                    <p>&copy; {new Date().getFullYear()} Quick Cooks. All rights reserved.</p>
+                </div>
+                <div>
+                    <img className="w-16" src="../media/imgs/Quick Cooks.png" alt="logo" />
+                </div>
+            </div>
+        </footer>
+    )
+}
